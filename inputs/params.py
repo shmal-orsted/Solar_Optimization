9bf5a7621c862fcbaf1f074d1cf5b3193c59edca
SHMAL <shmal@orsted.com>
2023-08-04 11:05:25 -0500

    Commiting the main section of the code, add environmental varaible for your API key and define a location for your weather file, neither are fully implemented yet

---
+++ inputs/params.py
@@ -0,0 +1,26 @@
+src_energy_manual_args = {
+            'Latitude': 34.23,
+            'Longitude': -103.1214,
+            'Altitude': 1229,
+            'ProjRefID': 'NW Blackwater',
+            'ACCapacity': 200000,
+            'DCCapacity': 0,
+            'Mounting': 'ground_1axis',
+            'IsFlushMount': False,
+            'Tilt': 60,
+            'Azimuth': 180,
+            'ModuleTech': 'cdte',
+            'IsBifacial': False,
+            'ModuleConfig': 'mod1P',
+            'InverterType': 'central',
+            'NumTransformerStages': 2,
+            'GCR': 0,
+            'DCDerate': 0.0,
+            'Availability': 1.0,
+            'Curtailment': 0.00,
+            'MonthlySoilingLoss': '0.02;0.017;0.01;0.01;0.0;0.0;0.01;0.01;0.01;0.01;0.01;0.014',
+            'MonthlyAlbedo': '0.4;0.4;0.3;0.2;0.2;0.2;0.2;0.2;0.2;0.2;0.2;0.3'
+        }
+
+csv_filename = "inputs/sample_weather_data.csv"
+dat_filename = "insample_weather_data.dat"
--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import os, pandas as pd
+import requests
+import requests_cache
+from inputs.params import src_energy_manual_args as src_params
+from functions import api_call, plotting
+from inputs import params
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def main():
+    """
+    This project will call the SRC api for multiple configurations to run optimization efforts.
+    It will also produce analysis, visuals and data outputs from the returns from the API
+    :param name: SRC_Key - this is an enviromental variable in my configuration but users will need to supply either a
+    team or individual key to use this project, can't store this on Github
+    :return:
+    Data visualizations
+    Analysis information
+    etc. (TBD)
+    """
+
+    #TODO Input gathering (ini, gui, etc)
+    # this is just going to be another input file for now (params.py)
+    #may end up making another file for imports if this gets too messy
+
+    # api call url
+    src_energy_manual_url = r'https://api.src.dnv.com/api/site/energy-manual'
+
+    # weather data file - just working for csv atm, will be changes
+    weather_df = pd.read_csv(params.csv_filename, skiprows=3, names=['GHI', 'DHI', 'Tamb', 'Wspd'])
+
+    # define gcr range, enter two numbers, inclusive lower end and exclusive upper end for use in api call loop
+    gcr_range = [30, 45]
+
+    # define dc, inclusive lower end and exclusive upper end range for use in api call loop
+    dc_range = [200000, 300000]
+
+    #TODO Setting up a cache to avoid extra API calls when we don't need them
+
+    #TODO API Call
+    json_dict, json_list = api_call.api_call_func(os.environ["API_KEY"], src_energy_manual_url, src_params, weather_df, gcr_range, dc_range)
+
+    #TODO API Return Processing
+
+    #TODO Imaging and Visualization (pyplot Graphs)
+
+    plotting.plot_data(json_dict)
+
+    #TODO Export Data, summarized and organized clearly
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    #TODO GUI to do all this
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
